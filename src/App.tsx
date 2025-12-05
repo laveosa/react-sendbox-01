e89f1bb0ab4697c5f9abb9e6cf1c7ee67d8bdec3
Nik <laveosa@yahoo.com>
2025-12-05 11:59:49 +0200

    app configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import cs from './App.module.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={cs.appMain}>
+      <h1>React Sendbox </h1>
+      <span className={cs.innerText}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea eveniet
+        exercitationem, illum libero quisquam tempore tenetur velit? Accusamus
+        ad alias doloribus enim est fugiat magni officia quibusdam quis
+        similique. Beatae.
+      </span>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,62 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import checker from 'vite-plugin-checker';
+import vitePluginSvgr from 'vite-plugin-svgr';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+const srcPath = resolve(__dirname, 'src').replace(/\\/g, '/');
+const DEFAULT_PORT = 3000;
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const baseUrl = mode === 'production' ? env.VITE_APP_BASE_PATH : '/';
+  const proxyTarget = env.VITE_API_URL || `http://localhost:${DEFAULT_PORT}`;
+
+  return {
+    base: baseUrl,
+    publicDir: 'public',
+    plugins: [
+      react(),
+      checker({
+        typescript: true,
+      }),
+      vitePluginSvgr({
+        include: '**/*.svg',
+        exclude: /node-modules/,
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': srcPath,
+        '~': resolve(__dirname, './'),
+      },
+    },
+    server: {
+      port: DEFAULT_PORT,
+      proxy: {
+        '/api': {
+          target: proxyTarget,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+    css: {
+      modules: {
+        generateScopedName: '[name]__[local]--[hash:base64:5]',
+        localsConvention: 'camelCaseOnly',
+      },
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "${srcPath}/styles/mixins.scss" as *;\n@use "${srcPath}/styles/variables.scss" as vars;\n`,
+        },
+      },
+    },
+    build: {
+      chunkSizeWarningLimit: 2000,
+      minify: mode === 'production' ? 'terser' : false,
+    },
+  };
+});
